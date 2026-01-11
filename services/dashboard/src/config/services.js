17632c1bdca01e42584af3c7ab36c4fd6901b113
b0urgk <b0ur8@proton.me>
2026-01-11 15:02:54 +0100

    added import of env vars to cfg

--- services/dashboard/src/config/services.js
+++ services/dashboard/src/config/services.js
@@ -6,5 +6,7 @@ module.exports = {
     eventsInternal: "http://events:3000",
     payment: process.env.PAYMENT_URL || "http://localhost:3005",
     paymentInternal: "http://payment:3000",
+    adminInternal: process.env.ADMIN_INTERNAL_URL || 'http://admin:3000',
+    adminServiceSecret: process.env.ADMIN_SERVICE_SECRET || '',
     env: environment.env,
 };
